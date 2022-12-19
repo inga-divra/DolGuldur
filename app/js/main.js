12974ed20a9aa226c6d997312f06549f7e04368f
inga-divra <ingady@gmail.com>
2022-12-19 11:11:10 +0200

    slider updates

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,24 @@
+/* let firstIndex = 0;
+
+function autoSlide() {
+    setTimeout(autoSlide, 3000)
+
+    let pics;
+    const img = document.querySelectorAll('img');
+
+    for (pics = 0; pics < img.length; pics++) {
+        img[pics].style.display = "none";
+    }
+    firstIndex++;
+    if (firstIndex > img.length) {
+        firstIndex = 1;
+    }
+    img[firstIndex - 1].style.display = "block";
+}
+
+autoSlide(); */
+
+
 const swiper = new Swiper('.swiper', {
 
     spaceBetween: 30,
@@ -6,6 +27,7 @@ const swiper = new Swiper('.swiper', {
         delay: 2500,
         disableOnInteraction: false,
     },
+
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
