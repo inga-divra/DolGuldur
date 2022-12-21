edc01888eb2e5f72d301eace45e75ab5b597b0e0
inga-divra <ingady@gmail.com>
2022-12-21 17:49:45 +0200

    fixed slider, icons and styles for main index.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,20 @@
-/* let firstIndex = 0;
+$(function () {
 
-function autoSlide() {
-    setTimeout(autoSlide, 3000)
-
-    let pics;
-    const img = document.querySelectorAll('img');
-
-    for (pics = 0; pics < img.length; pics++) {
-        img[pics].style.display = "none";
-    }
-    firstIndex++;
-    if (firstIndex > img.length) {
-        firstIndex = 1;
-    }
-    img[firstIndex - 1].style.display = "block";
-}
-
-autoSlide(); */
+    $('.hero__bg').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        fade: true,
+        "cssEase": "linear",
+        "autoplaySpeed": 0,
+        "speed": 4000,
+        infinite: true
+    })
+});
 
 
+
+//Swiper
 const swiper = new Swiper('.swiper', {
 
     spaceBetween: 30,
