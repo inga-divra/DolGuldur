f17510b26746ca073f21303ea8cf0ff6339545d6
inga-divra <ingady@gmail.com>
2022-12-24 20:56:02 +0200

    popup animation

--- app/js/main.js
+++ app/js/main.js
@@ -23,19 +23,13 @@ $(function () {
         modal: document.querySelector("[data-modal]"),
     };
 
-    refs.openModalBtn.addEventListener("click", () => {
-        toggleModal;
-
-    });
+    refs.openModalBtn.addEventListener("click", (toggleModal));
     refs.closeModalBtn.addEventListener("click", toggleModal);
 
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
     }
 
-    function showBackdrop() {
-        refs.modal.classList.add("show")
-    }
 })
     ();
 
